015e56d438c19f73ec0849f3f8872814145c995e
AlaaFallouh <awfallouh@yahoo.com>
2026-08-30 23:30:32 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,14 @@
+  
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a857cd2dc43063449f08c5d/1k0cmv5op';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+
---
+++ js/secript.js
@@ -0,0 +1,13 @@
+
+let icon = document.getElementById('icon-mobile');
+let ul = document.getElementById('ul-mobile');
+
+icon.onclick = function () {
+    ul.classList.toggle('mobile-nav1');
+}
+ 
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) { 
+        ul.classList.remove('mobile-nav1');
+    }
+});
